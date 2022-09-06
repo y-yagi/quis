d17dd4a39d2e819bf59a1a5210903bbc391fec2a
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-09-07 08:21:07 +0900

    Rename directory name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { supabase } from "../utils/supabaseClient";
+import { supabase } from "../lib/supabaseClient";
 import Auth from "../components/Auth";
 import { AuthSession } from "@supabase/supabase-js";
 import Layout from "../components/Layout";
