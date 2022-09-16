191c844234697f86cb25c54ffa4da892b16065ce
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-09-16 11:14:44 +0900

    Support basic auth

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export const middleware = (req: NextRequest) => {
+  if (
+    process.env.BASIC_AUTH_USER === null ||
+    process.env.BASIC_AUTH_PASSWORD === null
+  ) {
+    return NextResponse.next();
+  }
+
+  const basicAuth = req.headers.get("authorization");
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(" ")[1];
+    const [user, pwd] = atob(authValue).split(":");
+
+    if (
+      user === process.env.BASIC_AUTH_USER &&
+      pwd === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      return NextResponse.next();
+    }
+  }
+
+  const url = req.nextUrl;
+  url.pathname = "/api/auth";
+  return NextResponse.rewrite(url);
+};
