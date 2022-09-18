31f9e28aaa8a1561a56c16b5193ffeda309a6e31
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-09-19 07:54:45 +0900

    Adjust style

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div className="relative bg-white">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6">
+      <div className="mx-auto max-w-full px-4 sm:px-6">
         <div className="flex items-center justify-between border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
             <Link href="/">
