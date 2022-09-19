a7c13726ba5dbc6d0b04c811973d2d9bf902c051
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-09-20 07:46:00 +0900

    Specify id for auth form

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -26,6 +26,7 @@ export default function Auth() {
       <div>
         <input
           className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          id="quis_email"
           type="email"
           placeholder="Your email"
           value={email}
