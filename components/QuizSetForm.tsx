89753e860c91f3b295465485f962b392993c96b6
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-09-28 09:07:34 +0900

    Adjust form size

--- components/QuizSetForm.tsx
+++ components/QuizSetForm.tsx
@@ -62,7 +62,7 @@ const QuizSetForm: NextPage<Props> = ({ quizSet }) => {
         QuizSet
       </h3>
       <ErrorMessage message={errmsg} />
-      <form onSubmit={handleSubmit} className="w-full max-w-sm">
+      <form onSubmit={handleSubmit} className="w-full max-w-md">
         <div className="md:flex md:items-center mb-6">
           <div className="md:w-1/6">
             <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
