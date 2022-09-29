6e6b84f2d3236c8a04bf98a06276bfc9b5941454
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-09-29 16:19:21 +0900

    Add a test for `ErrorMessage` component

--- components/ErrorMessage.tsx
+++ components/ErrorMessage.tsx
@@ -12,7 +12,9 @@ const ErrorMessage = ({ message }: Props) => {
       className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
       role="alert"
     >
-      <strong className="font-bold">{message}</strong>
+      <strong className="font-bold" data-testid="error-message">
+        {message}
+      </strong>
     </div>
   );
 };
