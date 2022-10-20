55726fb540c4d5a62a0456f75ad0ec9df979cf31
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2022-10-20 11:37:16 +0900

    Add more tests for quiz sets

--- integration_tests/quiz_sets.spec.ts
+++ integration_tests/quiz_sets.spec.ts
@@ -1,12 +1,21 @@
 import { test, expect } from "@playwright/test";
 import { testServer } from "./helper";
 
-test("create a new quiz", async ({ page }) => {
+test("manage quiz set", async ({ page }) => {
   page.on("console", (msg) => console.log(msg.text()));
-
   await page.goto(testServer);
+
   await page.getByText("Create a new Quiz Sets").click();
-  await page.getByPlaceholder("name").fill("MyNewQuiz");
+  await page.getByPlaceholder("name").fill("MyNewQuizSet");
   await page.getByRole("button").click();
-  await expect(page.locator("table")).toContainText("MyNewQuiz");
+  await expect(page.locator("table")).toContainText("MyNewQuizSet");
+
+  await page.getByText("Edit").click();
+  await page.getByPlaceholder("name").fill("MyUpdatedQuizSet");
+  await page.getByRole("button").click();
+  await expect(page.locator("table")).toContainText("MyUpdatedQuizSet");
+
+  page.on("dialog", (dialog) => dialog.accept());
+  await page.getByText("Destroy").click();
+  await expect(page.locator("table")).not.toContainText("MyUpdatedQuizSet");
 });
